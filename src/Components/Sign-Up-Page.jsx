2f087f0e55f8189adfafbf387eb2d0bc26fe6083
Anita-Kumari1987 <anita.kumari1987@yahoo.com>
2025-03-05 09:41:34 +0100

    Initial commit - React setup

--- src/Components/Sign-Up-Page.jsx
+++ src/Components/Sign-Up-Page.jsx
@@ -2,16 +2,23 @@ import MemberLogInClick from "./MemberLog";
 import SignUpButton from "./Sign-Up-Button";
 import InputField from "./InputField";
 import React from "react";
+import "../Styles/Sign-Up-Page.css";
+import WebsiteName from "./WebsiteName";
 
 const SignUpPage = () => {
   return (
     <div className="signUpForm-Container">
-      <h1 className="tagLine">All of you. in one place</h1>
-      <div className="form-content">
-        <h3>Sign up now</h3>
-        <InputField />
-        <MemberLogInClick />
-        <SignUpButton />
+      <div className="left-Half">
+        <h1 className="tagLine">All of you. in one place</h1>
+        <div className="form-content">
+          <h3>Sign up now</h3>
+          <InputField />
+          <MemberLogInClick />
+          <SignUpButton />
+        </div>
+      </div>
+      <div className="right-Half">
+        <h1 className="website_Name">Healio</h1>
       </div>
     </div>
   );
---
+++ src/Components/WebsiteName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../Styles/WebsiteName.css";
+
+const WebsiteName = () => {
+  return <h1 className="website_Name">Healio</h1>;
+};
+export default WebsiteName;
